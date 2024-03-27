e3f6688247f906a1ff76d99e039f73dd1a072068
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-03-28 01:19:20 +0800

    Add target dependencies

--- Package.swift
+++ Package.swift
@@ -35,7 +35,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RHStackCard"),
+            name: "RHStackCard",
+            dependencies: ["RHNetworkAPI", "RHCacheStoreAPI", "RHUIComponent", "RHInterface", "SnapKit"]),
         .testTarget(
             name: "RHStackCardTests",
             dependencies: ["RHStackCard"]),
