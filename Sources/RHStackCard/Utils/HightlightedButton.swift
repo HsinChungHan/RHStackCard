f6850712b682b51f5d23863deb7fd1523bc5985f
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-03-28 01:19:34 +0800

    Implement `HightlightedButton`

--- Sources/RHStackCard/Utils/HightlightedButton.swift
+++ Sources/RHStackCard/Utils/HightlightedButton.swift
@@ -1,8 +1,52 @@
 //
-//  File.swift
-//  
+//  HightlightedButton.swift
+//  Tinder
 //
-//  Created by Chung Han Hsin on 2024/3/28.
+//  Created by Chung Han Hsin on 2023/6/7.
 //
 
-import Foundation
+import UIKit
+
+class HightlightedButton: UIButton {
+    let hightlightedColor: UIColor
+    let normalColor: UIColor
+    
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? hightlightedColor : normalColor
+            if isHighlighted {
+                setSizeAnimation(scaleX: 2)
+            } else {
+                transform = .identity
+            }
+        }
+    }
+    
+    init(normalColor: UIColor, hightlightedColor: UIColor) {
+        self.normalColor = normalColor
+        self.hightlightedColor = hightlightedColor
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setSizeAnimation(scaleX: CGFloat, duration: TimeInterval = 0.1) {
+        UIView.animate(withDuration: duration, delay: 0, options: [.curveEaseInOut], animations: {
+            let sizeTransformation = CGAffineTransform(scaleX: scaleX, y: scaleX)
+            self.transform = sizeTransformation
+        })
+    }
+    
+    func setIdentityAnimation(duration: TimeInterval = 0.25, completion: @escaping () -> Void) {
+        UIView.animate(withDuration: duration, delay: 0, options: [.curveEaseInOut], animations: {
+            self.transform = .identity
+            self.backgroundColor = self.backgroundColor?.withAlphaComponent(0)
+        }, completion: { isFinished in
+            if isFinished {
+                completion()
+            }
+        })
+    }
+}
