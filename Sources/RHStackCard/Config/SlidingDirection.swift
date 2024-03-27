7178d06f0b1bcb2ff77c7c470fe73d321cb06621
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-03-28 01:23:20 +0800

    Implement `SlidingDirection`

---
+++ Sources/RHStackCard/Config/SlidingDirection.swift
@@ -0,0 +1,55 @@
+//
+//  SlidingAction.swift
+//  SlidingCard
+//
+//  Created by Chung Han Hsin on 2024/3/28.
+//
+
+import Foundation
+enum SlidingDirection {
+    case toLeft
+    case toRight
+    case toTop
+    case backToIdentity
+    
+    enum Constant {
+        enum Slide {
+            static let xThrehold: CGFloat = 80
+            static let yThrehold: CGFloat = 140 // superLike 需要的 threhold 比較大
+        }
+    }
+    
+    static func getSwipeAwayDirection(with translation: CGPoint) -> Self {
+        let translationXDirection = translation.x
+        let translationYDirection = translation.y
+        
+        let shouldDismissedCard = abs(translationXDirection) > Constant.Slide.xThrehold || abs(translationYDirection) > Constant.Slide.yThrehold
+        
+        if shouldDismissedCard {
+            return SlidingDirection.getSlideDirection(with: translation)
+        }
+        return .backToIdentity
+    }
+    
+    static func getSlideDirection(with translation: CGPoint) -> Self {
+        let translationXDirection = translation.x
+        let translationYDirection = translation.y
+        if translationYDirection < -15  && abs(translationXDirection) < 100 { return .toTop }
+        if translationXDirection > 15 { return .toRight }
+        if translationXDirection < -15 { return .toLeft }
+        return .backToIdentity
+    }
+    
+    var swipeAwayTranslationValue: CGPoint {
+        switch self {
+        case .toRight:
+            return .init(x: 700, y: 0)
+        case .toTop:
+            return .init(x: 0, y: -700)
+        case .toLeft:
+            return .init(x: -700, y: 0)
+        case .backToIdentity:
+            return .init(x: 0, y: 0)
+        }
+    }
+}
