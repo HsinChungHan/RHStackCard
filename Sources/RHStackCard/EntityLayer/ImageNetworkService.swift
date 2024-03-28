307a24f445d4fc4f086a617468cadab8e4b4fa52
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-03-28 15:30:50 +0800

    Implement `ImageNetworkService`

---
+++ Sources/RHStackCard/EntityLayer/ImageNetworkService.swift
@@ -0,0 +1,41 @@
+//
+//  ImageNetworkService.swift
+//  SlidingCard
+//
+//  Created by Chung Han Hsin on 2024/3/25.
+//
+
+import Foundation
+import RHNetworkAPI
+
+protocol ImageNetworkServiceProtocol {
+    func downloadImage(with path: String, completion: @escaping (Result<Data, Error>) -> Void)
+}
+
+class ImageNetworkService: ImageNetworkServiceProtocol {
+    let factory = RHNetworkAPIImplementationFactory()
+    var networkAPI: RHNetworkAPIProtocol? = nil
+    let headers: [String : String] =
+    [
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"
+    ]
+    init(domainURL: URL?) {
+        guard let domainURL else { return }
+        networkAPI = factory.makeNonCacheClient(with: domainURL, headers: headers)
+    }
+    
+    func downloadImage(with path: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        networkAPI?.get(path: path, queryItems: []) { result in
+            switch result {
+            case let .success(data, _):
+                completion(.success(data))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
+extension ImageNetworkService {
+    
+}
